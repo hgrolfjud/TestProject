6330b0ca0c7c69276bdd36fbbe79ffa3a7ee7b65
hgrolfjud <hgrolfjud@gmail.com>
2019-07-30 13:24:00 +0300

    Finished

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,15 +1,18 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 
 import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 
 public class BaseTest {
     public static WebDriver driver;
-    public static WebDriverWait wait;
-
 
     public BaseTest(){
         WebDriverManager.chromedriver().setup();
@@ -17,16 +20,49 @@ public class BaseTest {
         settingDriver(driver);
     }
 
-    void goToURL(String url) {
+    void openPage(String url) {
         driver.get(url);
     }
-
 
     private static void settingDriver(WebDriver driver) {
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+    }
+
+    public void moveToElement(String elementLocation) {
+        moveToElement(elementLocation, 0, 0);
+    }
+
+    public void moveToElement(String elementLocation, int xOffset, int yOffset) {
+        Actions builder = new Actions(driver);
+        builder.moveToElement(driver.findElement(By.xpath(elementLocation)), xOffset, yOffset).build().perform();
+    }
+
+    public boolean isElementWithClass(String baseElement, String activeClass) {
+        return driver.findElement(By.xpath(baseElement)).findElements(By.xpath(".//*[contains(@class,'" + activeClass + "')]")).size() > 0;
+    }
+
+    public boolean isElementPresent(String elementLocation) {
+        try {
+            driver.findElement(By.xpath(elementLocation));
+            return true;
+        }
+        catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public void colorAndBacgroundColorAssert(String color, String background){
+        try{
+            sleep(500);
+        }catch (InterruptedException ex){
+            ex.printStackTrace();
+        }
+
+        Assert.assertTrue(isElementWithClass(color, "black"));
+        Assert.assertTrue(isElementPresent(background));
     }
 
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,14 +4,43 @@ import org.testng.annotations.BeforeTest;
 import static java.lang.Thread.sleep;
 
 public class Test extends BaseTest {
+    public static String letterR = "//*[@id='R']";
+    public static String letterG = "//*[@id='G']";
+    public static String letterB = "//*[@id='B']";
+
+    public static String redColorBackground = "//body[@class='r']";
+    public static String greenColorBackground = "//body[@class='g']";
+    public static String blueColorBackground = "//body[@class='b']";
+
+
     @BeforeTest
-    public void preConditionStep() throws InterruptedException{
-        goToURL("https://www.google.com/");
-        sleep(2000);
+    public void beforeTestStep() throws InterruptedException{
+        openPage("https://www.rrrgggbbb.com/");
+        sleep(1000);
+    }
+
+    @org.testng.annotations.Test
+    public void mainTest() throws InterruptedException{
+        int xMove = 15;
+        int yMove = 15;
+
+        //for red letter
+        moveToElement(letterR, xMove, yMove);
+        colorAndBacgroundColorAssert(letterR, redColorBackground);
+
+        //for green letter
+        moveToElement(letterG,  xMove, yMove);
+        colorAndBacgroundColorAssert(letterG, greenColorBackground);
+
+        //for blue letter
+        moveToElement(letterB, xMove, yMove);
+        colorAndBacgroundColorAssert(letterB, blueColorBackground);
     }
 
     @AfterTest
-    public void postConditionStep() {
+    public void afterTestStep() {
         driver.quit();
     }
+
+
 }
